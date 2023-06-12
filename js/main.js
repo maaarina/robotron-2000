9b7778b7c74cfd803f564d93451454f1887a7968
Marina Gabrielly <marina.gabri3lly@gmail.com>
2023-06-12 15:19:34 -0300

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
+
+const controle = document.querySelectorAll(".controle-ajuste")
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent) //a função manipula dados espera um texto (textContent) chamando + ou -
+    })
+})
+
+function manipulaDados (operacao) {
+    if (operacao === "-") {
+        braco.value = parseInt(braco.value) - 1
+    } 
+    else {
+        braco.value = parseInt(braco.value) + 1
+    }
+} 
