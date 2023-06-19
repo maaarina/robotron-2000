0add8408987fe3395923ff258ebe5d935e271df8
Marina Gabrielly <marina.gabri3lly@gmail.com>
2023-06-18 22:16:13 -0300

    Update data-att.js

--- exercicios/data-att.js
+++ exercicios/data-att.js
@@ -1,1 +1,11 @@
 // Você irá criar um botão que, ao ser clicado, faz aparecer esta lista, e também criará um segundo botão que, ao ser clicado, faz com que a lista de cores volte a sumir.
+
+function mostrarLista() {
+    var lista = document.getElementById("lista");
+    lista.style.display = "block";
+}
+
+function ocultarLista() {
+    var lista = document.getElementById("lista");
+    lista.style.display = "none";
+}
